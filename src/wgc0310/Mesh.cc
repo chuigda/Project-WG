2384e60cdefb3345198eeb9897f6fbb61d46945b
icey <icey@icey.tech>
2022-11-16 16:08:30 +0800

    模型可以动态替换，所以不放进 `resc` 文件夹

--- src/wgc0310/Mesh.cc
+++ src/wgc0310/Mesh.cc
@@ -34,31 +34,31 @@ WGCMeshCollection::WGCMeshCollection(GLFunctions *f, cw::DrawableArena &arena) {
   const auto blackPlastic = cw::GetBlackPlasticMaterial();
   const auto glass = cw::GetGlassMaterial();
 
-  LOAD_MESH_MTL("./resc/model/wheel.mesh", blackPlastic, wheel)
-  LOAD_MESH_MTL("./resc/model/monitor.mesh", plastic, monitor)
-  LOAD_MESH_MTL("./resc/model/monitor-intake.mesh", chrome, monitorIntake)
+  LOAD_MESH_MTL("./model/wheel.mesh", blackPlastic, wheel)
+  LOAD_MESH_MTL("./model/monitor.mesh", plastic, monitor)
+  LOAD_MESH_MTL("./model/monitor-intake.mesh", chrome, monitorIntake)
 
-  LOAD_MESH_MTL("./resc/model/chest-box.mesh", plastic, chestBox)
-  LOAD_MESH_MTL("./resc/model/chest-plate.mesh", chrome, chestPlate)
-  LOAD_MESH_MTL("./resc/model/power.mesh", blackPlastic, power)
-  LOAD_MESH_MTL("./resc/model/power-pin.mesh", brass, powerPin)
-  LOAD_MESH_MTL("./resc/model/ber-shell.mesh", glass, berShell)
-  LOAD_MESH("./resc/model/ber.mesh", ber)
+  LOAD_MESH_MTL("./model/chest-box.mesh", plastic, chestBox)
+  LOAD_MESH_MTL("./model/chest-plate.mesh", chrome, chestPlate)
+  LOAD_MESH_MTL("./model/power.mesh", blackPlastic, power)
+  LOAD_MESH_MTL("./model/power-pin.mesh", brass, powerPin)
+  LOAD_MESH_MTL("./model/ber-shell.mesh", glass, berShell)
+  LOAD_MESH("./model/ber.mesh", ber)
 
-  LOAD_MESH_MTL("./resc/model/abdomen.mesh", blackPlastic, abdomen)
-  LOAD_MESH_MTL("./resc/model/waist.mesh", plastic, waist)
+  LOAD_MESH_MTL("./model/abdomen.mesh", blackPlastic, abdomen)
+  LOAD_MESH_MTL("./model/waist.mesh", plastic, waist)
 
-  LOAD_MESH_MTL("./resc/model/shoulder-connector.mesh", plastic, shoulder)
-  LOAD_MESH_MTL("./resc/model/shoulder-plate.mesh", chrome, shoulderPlate)
+  LOAD_MESH_MTL("./model/shoulder-connector.mesh", plastic, shoulder)
+  LOAD_MESH_MTL("./model/shoulder-plate.mesh", chrome, shoulderPlate)
 
-  LOAD_MESH_MTL("./resc/model/big-arm.mesh", chrome, bigArm)
-  LOAD_MESH_MTL("./resc/model/big-arm-cover.mesh", plastic, bigArmCover)
-  LOAD_MESH_MTL("./resc/model/big-arm-connector.mesh", steel, bigArmConnector)
-  LOAD_MESH_MTL("./resc/model/small-arm.mesh", chrome, smallArm)
-  LOAD_MESH_MTL("./resc/model/small-arm-cover.mesh", plastic, smallArmCover)
-  LOAD_MESH_MTL("./resc/model/wheel-small.mesh", blackPlastic, wheelSmall)
-  LOAD_MESH_MTL("./resc/model/claw.mesh", steel, claw)
-  LOAD_MESH_MTL("./resc/model/claw-cover.mesh", plastic, clawCover)
+  LOAD_MESH_MTL("./model/big-arm.mesh", chrome, bigArm)
+  LOAD_MESH_MTL("./model/big-arm-cover.mesh", plastic, bigArmCover)
+  LOAD_MESH_MTL("./model/big-arm-connector.mesh", steel, bigArmConnector)
+  LOAD_MESH_MTL("./model/small-arm.mesh", chrome, smallArm)
+  LOAD_MESH_MTL("./model/small-arm-cover.mesh", plastic, smallArmCover)
+  LOAD_MESH_MTL("./model/wheel-small.mesh", blackPlastic, wheelSmall)
+  LOAD_MESH_MTL("./model/claw.mesh", steel, claw)
+  LOAD_MESH_MTL("./model/claw-cover.mesh", plastic, clawCover)
 
   #undef LOAD_MESH_MTL
 }
