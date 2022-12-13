8709609a953f14df9158c70af05a018ff41fc18d
Cousin Ze <alex@ckx-ily.cn>
2022-12-13 11:34:16 +0800

    Haha

---
+++ include/Version.h
@@ -0,0 +1,19 @@
+#ifndef PROJECT_WG_VERSION_H
+#define PROJECT_WG_VERSION_H
+
+#define WG_PRODUCT_NAME "Project-WG"
+#define WG_PRODUCT_VERSION 0,3,1,0
+#define WG_PRODUCT_VERSION_STRING "0.3.1.0"
+
+#define CWGLX_FILE_VERSION 0,3,0,0
+#define CWGLX_FILE_VERSION_STRING "0.3.0.0"
+
+#define CWUTIL_FILE_VERSION 0,3,0,0
+#define CWUTIL_FILE_VERSION_STRING "0.3.0.0"
+
+#ifdef CW_WIN32_RESOURCE
+#define COMPANY_NAME "7th General Design Group"
+#define COPYRIGHT_INFO "Copyright (C) 2022-2023 Chuigda"
+#endif // CW_WIN32_RESOURCE
+
+#endif // PROJECT_WG_VERSION_H
