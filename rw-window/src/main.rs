a76b85e35502e795f474d9a24ada65a2d040140c
icey <icey@icey.tech>
2023-04-18 22:04:08 +0800

    准备使用 tokio

---
+++ rw-window/src/main.rs
@@ -0,0 +1,32 @@
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::VulkanLibrary;
+use vulkano_win::VkSurfaceBuild;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{EventLoop, ControlFlow};
+use winit::window::WindowBuilder;
+
+fn main() {
+    let library = VulkanLibrary::new().expect("Unable to load local Vulkan library/DLL");
+    let required_extensions = vulkano_win::required_extensions(&library);
+    let vk_instance = Instance::new(library, InstanceCreateInfo {
+        enabled_extensions: required_extensions,
+        ..Default::default()
+    }).expect("Unable to create Vulkan instance");
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, vk_instance.clone())
+        .unwrap();
+
+    event_loop.run(|event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            },
+            _ => ()
+        }
+    });
+}
